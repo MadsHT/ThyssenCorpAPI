0dd3a4c9e462720a5d5610feb632db2a6201388d
MadsHT <madsthyssen@live.dk>
2019-05-24 16:41:27 +0200

    hello world

--- ThyssenCorpAPI/Controllers/FortniteController.cs
+++ ThyssenCorpAPI/Controllers/FortniteController.cs
@@ -27,7 +27,6 @@ namespace ThyssenCorpAPI.Controllers
         [Route("compare/{inputString}")]
         public ActionResult<JArray> GetStatsCompare(string inputString)
         {
-            return new ActionResult<JArray>(new JArray(inputString));
             
             string[] inputStringSplit = inputString.Split('-');
 
@@ -41,7 +40,7 @@ namespace ThyssenCorpAPI.Controllers
                 username2 = inputStringSplit[2];
             }
 
-            //return FSH.GetPlayerComparedStats(compareTo, username, username2);
+            return FSH.GetPlayerComparedStats(compareTo, username, username2);
         }
 
         [HttpGet]
