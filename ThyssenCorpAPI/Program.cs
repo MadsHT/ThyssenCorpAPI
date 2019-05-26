3282849dfb79413264cb8b26404fd1fbf5538d29
MadsHT <madsthyssen@live.dk>
2019-05-26 22:25:03 +0200

    ready for ip change

--- ThyssenCorpAPI/Program.cs
+++ ThyssenCorpAPI/Program.cs
@@ -32,7 +32,7 @@ namespace ThyssenCorpAPI
             {
                 host = new WebHostBuilder()
                     .UseKestrel()
-                    .UseUrls("http://192.168.0.32")
+                    .UseUrls("http://192.168.0.19")
                     .UseContentRoot(Directory.GetCurrentDirectory())
                     .UseStartup<Startup>()
                     .Build();
